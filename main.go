7cbd47c689931f1da1cb1e485aef9a0df8317bfd
ZennAdhyaksa <152697511+ZennAdhyaksa@users.noreply.github.com>
2026-05-31 20:58:59 +0700

    organize each function and initialize function

--- main.go
+++ main.go
@@ -14,6 +14,94 @@ type Movie struct {
 	Genre       string
 }
 
+// Find min/max function
+func findMin(arr []int) int {
+	min = arr[0]
+	for i = 1; i < len(arr); i++ {
+		if arr[i] < min {
+			min = arr[i]
+		}
+	}
+	return min
+}
+
+func findMax(arr []int) int {
+	max = arr[0]
+	for i = 1; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	return max
+}
+
+// Base sorting functions
+func selectionSort(arr []int) {
+	n := len(arr)
+	for i = 0; i < n-1; i++ {
+		minIdx = i
+		for j = i + 1; j < n; j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
+	}
+}
+
+func insertionSort(arr []int) {
+	n = len(arr)
+	for i = 1; i < n; i++ {
+		key := arr[i]
+		j = i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+}
+
+
+
+
+
+
+
+// Program functions
+func showAllMovies() {
+
+}
+
+func addMovie() {
+
+}
+
+func editMovie() {
+
+}
+
+func deleteMovie() {
+
+}
+
+func searchMenu() {
+
+}
+
+func statisticsMenu() {
+
+}
+
+func sortMenu() {
+
+}			
+
+
+
+
+
+// Main function
 func main() {
 	totalMovies = 0
 
@@ -72,49 +160,4 @@ func main() {
 
 
 
-func findMin(arr []int) int {
-	min = arr[0]
-	for i = 1; i < len(arr); i++ {
-		if arr[i] < min {
-			min = arr[i]
-		}
-	}
-	return min
-}
-
-func findMax(arr []int) int {
-	max = arr[0]
-	for i = 1; i < len(arr); i++ {
-		if arr[i] > max {
-			max = arr[i]
-		}
-	}
-	return max
-}
-
-func selectionSort(arr []int) {
-	n := len(arr)
-	for i = 0; i < n-1; i++ {
-		minIdx = i
-		for j = i + 1; j < n; j++ {
-			if arr[j] < arr[minIdx] {
-				minIdx = j
-			}
-		}
-		arr[i], arr[minIdx] = arr[minIdx], arr[i]
-	}
-}
-
-func insertionSort(arr []int) {
-	n = len(arr)
-	for i = 1; i < n; i++ {
-		key := arr[i]
-		j = i - 1
-		for j >= 0 && arr[j] > key {
-			arr[j+1] = arr[j]
-			j--
-		}
-		arr[j+1] = key
-	}
-}
 
