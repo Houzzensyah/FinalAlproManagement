88c6025c5c0ca22eddb9b40f6a6c48545f5cae5e
Aura Agny <auraagnyarp@gmail.com>
2026-06-01 22:58:51 +0700

    upgrade sikit min max & revisi sorting

--- main.go
+++ main.go
@@ -15,56 +15,65 @@ type Movie struct {
 }
 
 // Find min/max function
-// func findMin(arr []int) int {
-// 	min = arr[0]
-// 	for i = 1; i < len(arr); i++ {
-// 		if arr[i] < min {
-// 			min = arr[i]
-// 		}
-// 	}
-// 	return min
-// }
+func findMinGenre(arr []Movie, start, n int) int {
+	minIdx := start
+	for i := start + 1; i < n; i++ {
+		if arr[i].Genre < arr[minIdx].Genre {
+			minIdx = i
+		}
+	}
+	return minIdx
+}
 
 // func findMax(arr []int) int {
-// 	max = arr[0]
-// 	for i = 1; i < len(arr); i++ {
-// 		if arr[i] > max {
-// 			max = arr[i]
-// 		}
-// 	}
-// 	return max
-// }
+func findMaxGenre(arr []Movie, start, n int) int {
+	maxIdx := start
+	for i := start + 1; i < n; i++ {
+		if arr[i].Genre > arr[maxIdx].Genre {
+			maxIdx = i
+		}
+	}
+	return maxIdx
+}
 
 // // Base sorting functions
-// func selectionSort(arr []int) {
-// 	n := len(arr)
-// 	for i = 0; i < n-1; i++ {
-// 		minIdx = i
-// 		for j = i + 1; j < n; j++ {
-// 			if arr[j] < arr[minIdx] {
-// 				minIdx = j
-// 			}
-// 		}
-// 		arr[i], arr[minIdx] = arr[minIdx], arr[i]
-// 	}
-// }
+func selectionSortGenre(arr []Movie, n int, asc bool) {
+	for i := 0; i < n-1; i++ {
+		var targetIdx int
+		if asc {
+			targetIdx = findMinGenre(arr, i, n)
+		} else {
+			targetIdx = findMaxGenre(arr, i, n)
+		}
+		arr[i], arr[targetIdx] = arr[targetIdx], arr[i]
+	}
+}
 
-// func insertionSort(arr []int) {
-// 	n = len(arr)
-// 	for i = 1; i < n; i++ {
-// 		key := arr[i]
-// 		j = i - 1
-// 		for j >= 0 && arr[j] > key {
-// 			arr[j+1] = arr[j]
-// 			j--
-// 		}
-// 		arr[j+1] = key
-// 	}
-// }
-
-
-
-
+func insertionSortJudul(arr []Movie, n int, asc bool) {
+	for i := 1; i < n; i++ {
+		key := arr[i]
+		j := i - 1
+		
+		for j >= 0 {
+			if asc {
+				if arr[j].Title > key.Title {
+					arr[j+1] = arr[j]
+					j--
+				} else {
+					break
+				}
+			} else {
+				if arr[j].Title < key.Title {
+					arr[j+1] = arr[j]
+					j--
+				} else {
+					break
+				}
+			}
+		}
+		arr[j+1] = key
+	}
+}
 
 
 
